c49dcd7eafb50921c4f3635323835965c375c844
Md Rabby <ablzerabbi159@gmail.com>
2023-11-18 16:27:51 +0600

    about company complete

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,29 @@
+
+
+const About = () => {
+  return (
+    <div>
+      <div
+        className="hero max-w-7xl mx-auto my-10"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/3C0TVy2/chef-service.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-opacity-30  p-52"></div>
+        <div className="hero-content text-center px-24 bg-slate-200">
+          <div className="max-w-md ">
+            <h1 className="mb-5 text-5xl font-bold">Bistro Boss</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import About from "../../Components/About/About";
 import Banner from "../../Components/Banner/Banner";
 import Categories from "../../Components/Categories/Categories";
 import Navbar from "../../Components/Navbar/Navbar";
@@ -9,6 +10,7 @@ const Home = () => {
             <Navbar></Navbar>
             <Banner></Banner>
             <Categories></Categories>
+            <About></About>
         </div>
     );
 };
